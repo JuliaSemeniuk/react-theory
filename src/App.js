3de8ef2eb67a7f8e86b05f5319ea05a517fdd04a
Julia <j.semeniuk.v@gmail.com>
2020-11-03 14:57:08 +0200

    css-1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
+import LearnCSS from './CSS';
 
 import HelloWorld from './LearnJS/001_helloWorld';
 import Types from './LearnJS/003_types';
 import Review from './TProg/02_Review';
 import ObjectRecursion from './TProg/03_objectRecursion';
+//import LearnCSS from './CSS/index'
 
 function App() {
     return (
         <div className="App">
-            <Review />
+            {/* <Review /> */}
+            <LearnCSS/>
         </div>
     );
 }
