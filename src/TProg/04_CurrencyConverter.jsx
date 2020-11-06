7b77dfbbfa93c49ba9b20274f404eeb8305df075
Julia <j.semeniuk.v@gmail.com>
2020-11-06 11:00:29 +0200

    currency converter

--- src/TProg/04_CurrencyConverter.jsx
+++ src/TProg/04_CurrencyConverter.jsx
@@ -6,27 +6,41 @@ export default class CurrencyConverter extends React.Component {
        super(props);
 
        this.state = {
-           amount: 0,
+           amount: '',
+           currency: '$',
            result: '',
+            message: '€'
        };
    };
 
    onChangeAmount = (event) => {
        let currentAmount = event.target.value;
-       this.setState({amount: currentAmount});
+       let currentResult;
+       const { amount, currency } = this.state;
+
+       if (currency === '$') {
+           currentResult = currentAmount * 0.85;
+       } else {
+        currentResult = currentAmount * 1.18;
+       };
+       this.setState({amount: currentAmount, result: currentResult});
+       console.log(amount);
    };
 
    OnClickCurrency = (event) => {
+       let currentCurrency = event.target.value;
        let currentResult;
-       const { amount } = this.state;
-
-       if (event.target.value === '$') {
-           currentResult = parseInt(amount) * 0.85;
-       } else {
-        currentResult = amount * 1.18;
-       };
-       this.setState({result: currentResult});
-       console.log(amount)
+       let currentMessage;
+       const {amount} = this.state;
+       if (currentCurrency === '$') {
+            currentResult = amount * 0.85;
+            currentMessage = '€'
+            } else {
+            currentResult = amount * 1.18;
+            currentMessage = '$'
+        };
+       
+       this.setState({currency: currentCurrency, result: currentResult, message: currentMessage});       
    };
 
     render () {        
@@ -34,9 +48,10 @@ export default class CurrencyConverter extends React.Component {
         return (
             <div>
                <input onChange={this.onChangeAmount} type='text' placeholder='amount'></input>
-               <input onClick={this.OnClickCurrency} type='radio' name='currency' value='$'></input>$
-               <input onClick={this.OnClickCurrency} type='radio' name='currency' value='€'></input>€
-               <input onClick={this.OnClickCurrency} type='text' value={this.state.result}></input>
+               <input onClick={this.OnClickCurrency} type='radio' name='currency' value='$' checked={this.state.currency === '$'}></input>$
+               <input onClick={this.OnClickCurrency} type='radio' name='currency' value='€'checked={this.state.currency === '€'}></input>€
+               <input onClick={this.OnClickCurrency} type='text' value={this.state.result + this.state.message}></input>
+               
             </div>
         )
     }
