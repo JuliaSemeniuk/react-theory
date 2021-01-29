4f11e1898e046dc137afec41930a0d2c4217ed7c
Julia <j.semeniuk.v@gmail.com>
2021-01-29 14:52:31 +0200

    total amount of days

--- src/TProg/09_AgeCalculator.jsx
+++ src/TProg/09_AgeCalculator.jsx
@@ -33,11 +33,11 @@ function calculateLeapYears(birthYear, currentYear) {
 }
 
 function calculateAge(birthDataElemenets, currentDataElements, leapYears) {
+    // the total amount of days from from birth year to current
     const daysYears = (currentDataElements.year - birthDataElemenets.year - leapYears) * 365 + leapYears * 366
     console.log('days  years: ', daysYears)
 
-    let daysMonthInBirthYear = 0
-
+    // check of Leap year
     let isLeapYear = false
     if (
         (birthDataElemenets.year % 4 === 0 && birthDataElemenets.year % 100 != 0) ||
@@ -46,57 +46,65 @@ function calculateAge(birthDataElemenets, currentDataElements, leapYears) {
         isLeapYear = true
     }
 
+    // the amount of days in birth year
+
+    let daysInBirthYear = 0
     const day30 = [4, 6, 9, 11]
     const day31 = [1, 3, 5, 7, 8, 10, 12]
 
-    if (birthDataElemenets.month > currentDataElements.month) {
-        for (let i = birthDataElemenets.month; i < 13; i++) {
-            if (day30.includes(i)) {
-                daysMonthInBirthYear = daysMonthInBirthYear + 30
-                continue
-            }
-            if (day31.includes(i)) {
-                daysMonthInBirthYear = daysMonthInBirthYear + 31
-                continue
-            } else {
-                daysMonthInBirthYear = isLeapYear ? daysMonthInBirthYear + 29 : daysMonthInBirthYear + 28
-            }
+    for (let i = birthDataElemenets.month; i < 13; i++) {
+        if (day30.includes(i)) {
+            daysInBirthYear = daysInBirthYear + 30
+            continue
         }
-
-        for (let i = 1; i < currentDataElements.month + 1; i++) {
-            if (day30.includes(i)) {
-                daysMonthInBirthYear = daysMonthInBirthYear + 30
-                continue
-            }
-            if (day31.includes(i)) {
-                daysMonthInBirthYear = daysMonthInBirthYear + 31
-                continue
-            } else {
-                daysMonthInBirthYear = isLeapYear ? daysMonthInBirthYear + 29 : daysMonthInBirthYear + 28
-            }
-        }
-    } else {
-        for (let i = birthDataElemenets.month; i < currentDataElements.month + 1; i++) {
-            if (day30.includes(i)) {
-                daysMonthInBirthYear = daysMonthInBirthYear + 30
-                continue
-            }
-            if (day31.includes(i)) {
-                daysMonthInBirthYear = daysMonthInBirthYear + 31
-                continue
-            } else {
-                daysMonthInBirthYear = isLeapYear ? daysMonthInBirthYear + 29 : daysMonthInBirthYear + 28
-            }
+        if (day31.includes(i)) {
+            daysInBirthYear = daysInBirthYear + 31
+            continue
+        } else {
+            daysInBirthYear = isLeapYear ? daysInBirthYear + 29 : daysInBirthYear + 28
         }
     }
 
-    console.log('daysMonthInBirthYear: ', daysMonthInBirthYear)
+    const a = daysYears - (365 - daysInBirthYear)
+
+    console.log('days in birth year: ', daysInBirthYear)
+
+    // the amount of days in current year
+
+    let daysInCurrentYear = 0
+
+    for (let i = 1; i < currentDataElements.month; i++) {
+        if (day30.includes(i)) {
+            daysInCurrentYear = daysInCurrentYear + 30
+            continue
+        }
+        if (day31.includes(i)) {
+            daysInCurrentYear = daysInCurrentYear + 31
+            continue
+        } else {
+            daysInCurrentYear = isLeapYear ? daysInCurrentYear + 29 : daysInCurrentYear + 28
+        }
+    }
+
+    console.log('days in current year:', daysInCurrentYear)
+
+    let generalAmountOfDays
+
+    if (isLeapYear === true) {
+        generalAmountOfDays =
+            daysYears - (366 - daysInBirthYear) - birthDataElemenets.day + daysInCurrentYear + currentDataElements.day
+    } else {
+        generalAmountOfDays =
+            daysYears - (365 - daysInBirthYear) - birthDataElemenets.day + daysInCurrentYear + currentDataElements.day
+    }
+
+    console.log('general amount of Days:', generalAmountOfDays)
 }
 
 export default class AgeCalculator extends React.Component {
     render() {
-        const julieBirthData = '11.08.1991'
-        const currentData = '25.01.2021'
+        const julieBirthData = '11.08.1992'
+        const currentData = '25.03.2020'
 
         const birthDataElemenets = splitBirthData(julieBirthData) // return { day, month, year }
         const currentDataElements = splitCurrentData(currentData)
